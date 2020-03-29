a8b9fe4753e544b41733b93eb56d92f500b80fb6
Avinash <avinash.bhat@icfnext.com>
2020-03-29 17:04:56 +0530

    My Travel Tab added to site

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -108,6 +108,26 @@
         }
       }
     });
+    // Travel isotope and filter
+  $(window).on('load', function() {
+    var portfolioIsotope = $('.travel-container').isotope({
+      itemSelector: '.travel-item',
+      layoutMode: 'fitRows'
+    });
+
+    $('#travel-filters li').on('click', function() {
+      $("#travel-filters li").removeClass('filter-active');
+      $(this).addClass('filter-active');
+
+      portfolioIsotope.isotope({
+        filter: $(this).data('filter')
+      });
+    });
+    // Initiate venobox (lightbox feature used in My Travel)
+    $(document).ready(function() {
+      $('.venobox').venobox();
+    });
+  });
   // AOS Initialization
   AOS.init({
     duration: 1000,
